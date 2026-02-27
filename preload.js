6d7b979db0b98ddf49f79cdeb48b99b57cadb384
Drewdus <davisam42@gmail.com>
2026-02-26 21:30:18 -0500

    Add send method to ipcRenderer in preload.js

--- preload.js
+++ preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
     ipcRenderer: {
         invoke: (channel, data) => ipcRenderer.invoke(channel, data),
+        send: (channel, data) => ipcRenderer.send(channel, data),
         on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
         removeListener: (channel, func) => ipcRenderer.removeListener(channel, func)
     }
